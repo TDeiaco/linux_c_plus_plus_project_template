a558e677af2a2b335541845092a76f4920a92e50
Taylor Deiaco <tdeiaco@gmail.com>
2014-06-10 00:14:42 -0600

    First Commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+//**********************************************
+//
+//
+//
+// 
+//Use under the GLP License 
+//
+//**********************************************
+
+#include <iostream>
+
+using namespace std;
+
+
+
+int main(){
+
+  cout<< "Hello World!"<<endl;
+
+  return 0;
+}
